dd16a366b4f1c69496d3fa7cea0168a400f4d081
ThorKild_ <pro.antoine.masia@gmail.com>
2025-02-07 11:13:39 +0100

    sécuriser le mot de passe utilisateur avec le hachage bcrypt

--- app/controllers/user.py
+++ app/controllers/user.py
@@ -1,12 +1,16 @@
 from sqlalchemy.orm import Session
 from app.models.user import User
 from app.schemas.user import UserCreate, UserUpdate
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def create_user(db: Session, user: UserCreate):
+    hashed_password = pwd_context.hash(user.password)
     db_user = User(
         username=user.username,
         email=user.email,
-        password=user.password
+        password=hashed_password
     )
     db.add(db_user)
     db.commit()
